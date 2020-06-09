a537fd2d9c863955c73915c189c24025d7e7299c
Agarwal <trishal.agarwal@stryker.com>
2020-06-09 19:14:48 +0530

    Initial commit

---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	@Test
+	public void APITest()
+	{
+		System.out.println("Rest Automation");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void APITest()
+	{
+		System.out.println("Browser Automation");
+	}
+
+}
